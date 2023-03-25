83ff020c841afb64df8f8666d62bef43e337ddb0
Bhavye Mathur <bhavyemathur@gmail.com>
2023-03-25 22:51:31 +0530

    Added accented heading patterns

--- inc/register-block-patterns.php
+++ inc/register-block-patterns.php
@@ -9,6 +9,22 @@ function iispmun_register_patterns()
             "content" => file_get_contents(get_theme_file_path("patterns/mun-theme.html")),
         )
     );
+
+    register_block_pattern(
+        "iispmun/primary-accented-heading",
+        array(
+            "title" => __("Primary Accented Heading", "iispmun"),
+            "content" => file_get_contents(get_theme_file_path("patterns/primary-accented-heading.html")),
+        )
+    );
+
+    register_block_pattern(
+        "iispmun/secondary-accented-heading",
+        array(
+            "title" => __("Secondary Accented Heading", "iispmun"),
+            "content" => file_get_contents(get_theme_file_path("patterns/secondary-accented-heading.html")),
+        )
+    );
 }
 
 add_action("init", "iispmun_register_patterns");
