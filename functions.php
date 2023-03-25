a6aeff153cde664ed8b6fd6ce9fd8ed8aa61c95b
Bhavye Mathur <bhavyemathur@gmail.com>
2023-03-25 20:19:43 +0530

    Completed mun-theme.html

--- functions.php
+++ functions.php
@@ -4,6 +4,11 @@ add_theme_support( "menus" );
 add_theme_support( "custom-logo" );
 add_theme_support( "post-thumbnails" );
 
+remove_theme_support("core-block-patterns");
+
+add_theme_support( "editor-styles" );
+add_editor_style( "assets/css/main.css" );
+
 require_once get_theme_file_path( "inc/remove-admin-bar.php" );
 
 require_once get_theme_file_path( "inc/load-styles.php" );
@@ -20,5 +25,3 @@ require_once get_theme_file_path( "inc/register-iispmun_editions.php" );
 
 require_once get_theme_file_path( "inc/register-block-styles.php" );
 require_once get_theme_file_path( "inc/register-block-patterns.php" );
-
-require_once get_theme_file_path( "inc/remove-core-block-patterns.php" );
--- inc/remove-core-block-patterns.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function iispmun_remove_core_patterns()
-{
-    remove_theme_support("core-block-patterns");
-}
-
-add_action("init", "iispmun_remove_core_patterns");
