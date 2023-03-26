38730dd327515dc1292dcc93395028c355241ece
Bhavye Mathur <bhavyemathur@gmail.com>
2023-03-26 12:15:29 +0530

    Added hover card templates
    Signed-off-by: Bhavye Mathur <bhavyemathur@gmail.com>

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
+add_theme_support("align-wide");
 add_theme_support( "menus" );
 add_theme_support( "custom-logo" );
 add_theme_support( "post-thumbnails" );
--- inc/register-block-patterns.php
+++ inc/register-block-patterns.php
@@ -25,6 +25,38 @@ function iispmun_register_patterns()
             "content" => file_get_contents(get_theme_file_path("patterns/secondary-accented-heading.html")),
         )
     );
+
+    register_block_pattern(
+        "iispmun/hover-card",
+        array(
+            "title" => __("Hover Card", "iispmun"),
+            "content" => file_get_contents(get_theme_file_path("patterns/hover-card.html")),
+        )
+    );
+
+    register_block_pattern(
+        "iispmun/hover-card-with-image",
+        array(
+            "title" => __("Hover Card with Image", "iispmun"),
+            "content" => file_get_contents(get_theme_file_path("patterns/hover-card-with-image.html")),
+        )
+    );
+
+    register_block_pattern(
+        "iispmun/committee-hover-card",
+        array(
+            "title" => __("Committee Hover Card", "iispmun"),
+            "content" => file_get_contents(get_theme_file_path("patterns/committee-hover-card.html")),
+        )
+    );
+
+    register_block_pattern(
+        "iispmun/mun-committees",
+        array(
+            "title" => __("IISP MUN Committees", "iispmun"),
+            "content" => file_get_contents(get_theme_file_path("patterns/mun-committees.html")),
+        )
+    );
 }
 
 add_action("init", "iispmun_register_patterns");
